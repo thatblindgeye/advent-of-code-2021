67e5e0bc37d50ad6ae76350a43e8097a448c0999
thatblindgeye <thatblindgeye@gmail.com>
2021-12-08 18:43:54 -0500

    Create functions and test for day 3 part 1

---
+++ scripts/day-03/part1.js
@@ -0,0 +1,37 @@
+function createArraysByPosition(arr) {
+  const arrayByPosition = [];
+
+  for (let i = 0; i < arr[0].length; i++) {
+    arrayByPosition.push([]);
+    arr.forEach((item, index) => {
+      arrayByPosition[i].push(Number(arr[index][i]));
+    });
+  }
+
+  return arrayByPosition;
+}
+
+function reduceArrayItems(arr) {
+  return arr.map((itemArray) => {
+    return itemArray.reduce((previous, current) => {
+      if (current === 0) {
+        return previous - 1;
+      }
+
+      return previous + 1;
+    }, 0);
+  });
+}
+
+export default function getPowerConsumption(reportArray) {
+  const positionedArray = createArraysByPosition(reportArray);
+  const reducedArrays = reduceArrayItems(positionedArray);
+
+  const gammaArray = reducedArrays.map((item) => (item < 0 ? 0 : 1));
+  const gammaRate = gammaArray.join('');
+
+  const epsilonArray = reducedArrays.map((item) => (item < 0 ? 1 : 0));
+  const epsilonRate = epsilonArray.join('');
+
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+}
